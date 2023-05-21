d409d8028d330d63650e802742c6ad5da92e11d9
Ben Coleman <benc.uk@gmail.com>
2023-05-21 10:40:07 +0100

    Addresses severe JWKS refresh bug. Fixes #1

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -20,8 +20,8 @@ import (
 // JWTValidator is a struct that can be used to protect routes
 type JWTValidator struct {
 	clientID string
-	jwksURL  string
 	scope    string
+	jwks     *keyfunc.JWKS
 }
 
 type PassthroughValidator struct {
@@ -34,10 +34,21 @@ type Validator interface {
 
 // NewJWTValidator creates a new JWTValidator struct
 func NewJWTValidator(clientID string, jwksURL string, scope string) JWTValidator {
+	// Create and store the JWKS once, this will be refreshed automatically
+	jwks, err := keyfunc.Get(jwksURL, keyfunc.Options{
+		RefreshInterval: time.Duration(1) * time.Hour,
+	})
+
+	if err != nil {
+		log.Printf("### 🔐 Auth: Failed to fetch the JWKS. Error: %s", err)
+	} else {
+		log.Printf("### 🔐 Auth: Enabling auth, JWKS fetched from %s", jwksURL)
+	}
+
 	return JWTValidator{
 		clientID: clientID,
-		jwksURL:  jwksURL,
 		scope:    scope,
+		jwks:     jwks,
 	}
 }
 
@@ -48,7 +59,7 @@ func NewPassthroughValidator() PassthroughValidator {
 // Middleware returns middleware to enforce JWT auth on all routes
 func (v JWTValidator) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !validateRequest(r, v.clientID, v.jwksURL, v.scope) {
+		if !validateRequest(r, v.clientID, v.scope, v.jwks) {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
@@ -60,7 +71,7 @@ func (v JWTValidator) Middleware(next http.Handler) http.Handler {
 // Protect can be added around any route handler to enforce JWT auth
 func (v JWTValidator) Protect(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if !validateRequest(r, v.clientID, v.jwksURL, v.scope) {
+		if !validateRequest(r, v.clientID, v.scope, v.jwks) {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
@@ -84,7 +95,7 @@ func (v PassthroughValidator) Protect(next http.HandlerFunc) http.HandlerFunc {
 }
 
 // validateRequest is an internal function to validate a request
-func validateRequest(r *http.Request, clientID string, jwksURL string, scope string) bool {
+func validateRequest(r *http.Request, clientID string, scope string, jwks *keyfunc.JWKS) bool {
 	// Get auth header & bearer scheme
 	authHeader := r.Header.Get("Authorization")
 	if len(authHeader) == 0 {
@@ -101,18 +112,16 @@ func validateRequest(r *http.Request, clientID string, jwksURL string, scope str
 		return false
 	}
 
-	jwks, err := keyfunc.Get(jwksURL, keyfunc.Options{
-		RefreshInterval: time.Duration(1) * time.Hour,
-	})
-	if err != nil {
-		log.Printf("### Auth: Failed to get the JWKS. Error: %s", err)
+	// JWKS might not have been fetched or some other error with it, if not then deny access
+	if jwks == nil {
+		log.Printf("### 🔐 Auth: No JWKS, cannot validate token, denying access")
 		return false
 	}
 
 	// Parse the JWT string using the key fetched from the JWKS
 	token, err := jwt.Parse(authParts[1], jwks.Keyfunc)
 	if err != nil {
-		log.Printf("### Auth: Failed to parse the JWT. Error: %s", err)
+		log.Printf("### 🔐 Auth: Failed to parse the JWT. Error: %s", err)
 		return false
 	}
 
@@ -120,13 +129,13 @@ func validateRequest(r *http.Request, clientID string, jwksURL string, scope str
 
 	// Check the scope includes the app scope
 	if !strings.Contains(claims["scp"].(string), scope) {
-		log.Printf("### Auth: Scope '%s' is missing from token scope '%s'", scope, claims["scp"])
+		log.Printf("### 🔐 Auth: Scope '%s' is missing from token scope '%s'", scope, claims["scp"])
 		return false
 	}
 
 	// Check the token audience is the client id, this might have already been done by jwt.Parse
 	if claims["aud"] != clientID {
-		log.Printf("### Auth: Token audience '%s' does not match '%s'", claims["aud"], clientID)
+		log.Printf("### 🔐 Auth: Token audience '%s' does not match '%s'", claims["aud"], clientID)
 		return false
 	}
 
